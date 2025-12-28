68b50fbfc8ee1337eb4a1c88098d5a7976a5af06
poppyseedcake <w.makowiec@gmail.com>
2025-12-28 23:20:46 +0100

    pokedex cli

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,23 @@
+import { createInterface } from 'node:readline';
+
+export function startREPL(): void {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > "
+    });
+    rl.prompt();
+    rl.on("line", async (line) => {
+        const userInput = cleanInput(line);
+        if (userInput.length === 0) {
+            rl.prompt();
+            return;
+        }
+        console.log(`Your command was: ${userInput[0]}`);
+        rl.prompt();
+    });
+}
+
 export function cleanInput(input: string): string[] {
     return input
         .toLowerCase()
