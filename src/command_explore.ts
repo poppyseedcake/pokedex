ad52abc8827a4562347e9b65a565b18defdf1dfd
poppyseedcake <w.makowiec@gmail.com>
2026-01-13 22:53:31 +0100

    dadasdas

--- src/command_explore.ts
+++ src/command_explore.ts
@@ -4,5 +4,6 @@ export async function commandExplore(state: State, location: string) {
     const response = await state.pokeAPI.fetchLocation(location);
     console.log("explore");
     console.log(response);
+    console.log("explore");
 
 }
