0ae75278a99665f0b700547269de16fc8d367535
poppyseedcake <w.makowiec@gmail.com>
2026-01-22 17:31:53 +0100

    my catch

--- src/command_catch.ts
+++ src/command_catch.ts
@@ -7,6 +7,17 @@ export async function commandCatch(state: State, ...args: string[]) {
   
   const pokemon_name = args[0];
   const pokemon = await state.pokeAPI.fetchPokemon(pokemon_name);
-  
-  console.log(`Throwing a Pokeball at ${pokemon_name}...`);
+  const chance: number = (300 - pokemon.base_experience) / 300;
+  const isCatched = Math.random() < chance ? 1 : 0;
+
+  console.log(`Throwing a Pokeball at ${pokemon.name}...`);
+
+  if (isCatched) {
+    console.log(`${pokemon.name} catched!`);
+    state.pokemon[pokemon.name] = pokemon;
+
+  } else {
+    console.log(`Try again!`);
+
+  }
 }
